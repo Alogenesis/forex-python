0cceddb318aad8eae77104e44e9716becd3894d9
Alogenesis <nononosolo@hotmail.com>
2020-05-30 00:21:00 +0700

    Draft menu function

--- forex_python/Lecture114_Wini_N_Forex.py
+++ forex_python/Lecture114_Wini_N_Forex.py
@@ -42,6 +42,58 @@ def money_convert_bitcoin():    #in use
     b.convert_to_btc(5000, 'USD')
     print(b.convert_to_btc(5000, 'USD'))
 
+''' Menu Function '''
+def welcome_message():
+    print('------------------------------------------')
+    print('----------- Welcome To Coverter ----------')
+    print('----------- ------------------- ----------')
+    print('------- Please Select Menu Number --------')
+    print('------------------------------------------')
+
+def first_menu():
+    print('1. Currency converter')
+    print('2. Currency rate compare with all nowsdays')
+    print('3. History currency rate converter')
+    print('4. Bitcoin price nowsdays')
+    print('5. Values to bitcoin nowsdays')
+    print('------------------------------------------')
+
+def input_first_menu():
+    loop_set = 0
+    while loop_set == 0:
+        user_input = int(input('Menu Number >> '))
+        if user_input == 1:
+            print('Select Menu 1. Currency converter')
+            loop_set = 1
+            return 1
+
+        elif user_input == 2:
+            print('Select Menu 2. Currency rate compare with all nowsdays')
+            loop_set = 1
+            return 2
+        elif user_input == 3:
+            print('Select Menu 3. History currency rate converter')
+            loop_set = 1
+            return 3
+        elif user_input == 4:
+            print('Select Menu 4. Bitcoin price nowsdays')
+            loop_set = 1
+            return 4
+        elif user_input == 5:
+            print('Select Menu 5. Currency to bitcoin nowsdays')
+            loop_set = 1
+            return 5
+        else:
+            print('Invalid Menu Number, please select again')
+
+
+
+
+
+
+
+''' Test Part '''
+'''
 print("-" * 50)
 print('current_rate(USD)')
 print(current_rate())
@@ -69,7 +121,13 @@ print(one_bitcoin_price())
 print("-" * 50)
 print('money_convert_bitcoin(THB)')
 print(money_convert_bitcoin())
+'''
 
-
-#Menu Part
-print('')
+''' Menu Part '''
+welcome_message()
+first_menu()
+user_input = input_first_menu()
+if user_input == 1:
+    print('Going to Menu 1')
+elif user_input ==2:
+    print('Going to Menu 2')
